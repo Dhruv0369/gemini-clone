b5aab582c5fc5f8b721bc9cb434a47b30ae7f680
Dhruv0369 <148555616+Dhruv0369@users.noreply.github.com>
2024-10-22 22:09:00 +0530

    First commit

---
+++ src/config/gemini.js
@@ -0,0 +1,76 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Replace with your actual API key here
+const apiKey = "AIzaSyCLWj1DEWueGk8TxCSq_TT9W-cDh9mkrEk";  
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
+
+
+
+
+
+
+
+
+
+
+
+
+// import {
+//     GoogleGenerativeAI,
+//     HarmCategory,
+//     HarmBlockThreshold,
+//   } from "@google/generative-ai" ;
+  
+//   const apiKey = process.env.AIzaSyCLWj1DEWueGk8TxCSq_TT9W-cDh9mkrEk;
+//   const genAI = new GoogleGenerativeAI(apiKey);
+  
+//   const model = genAI.getGenerativeModel({
+//     model: "gemini-1.5-flash",
+//   });
+  
+//   const generationConfig = {
+//     temperature: 1,
+//     topP: 0.95,
+//     topK: 64,
+//     maxOutputTokens: 8192,
+//     responseMimeType: "text/plain",
+//   };
+  
+//   async function run(prompt) {
+//     const chatSession = model.startChat({
+//       generationConfig,
+//       history: [
+//       ],
+//     });
+  
+//     const result = await chatSession.sendMessage(prompt);
+//     console.log(result.response.text());
+//   }
+  
+//   export default run;
