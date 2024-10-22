890126c36be56bf17331f09ea8ce2ae1803ef52a
Dhruv0369 <148555616+Dhruv0369@users.noreply.github.com>
2024-10-22 22:33:17 +0530

    Minor Changes

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -33,7 +33,7 @@ const Main = () => {
         <div className='main'>
             <div className='nav'>
                 <p>Gemini</p>
-                <img src={assets.user_icon} alt="" />
+                <img src={assets.profile_pic} alt="" />
             </div>
             <div className="main-container">
                 {
@@ -64,7 +64,7 @@ const Main = () => {
                         </>
                         : <div className='result'>
                             <div className="result-title">
-                                <img src={assets.user_icon} alt="" />
+                                <img src={assets.profile_pic} alt="" />
                                 <p>{recentPrompt}</p>
                             </div>
                             <div className="result-data">
